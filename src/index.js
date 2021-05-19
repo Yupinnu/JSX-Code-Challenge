c8a37a44c3b4b039c0cf96d86240cb581f871f53
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2021-05-19 20:30:33 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>Food</li>
+      <li>Milk</li>
+      <li>Vodka</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
